0c3d41fc64127ce1617989cba6e9b419735b3446
ZEVS1206 <egor_bud5@inbox.ru>
2024-10-25 10:24:29 +0300

    Created structure with assembler parameters

--- Assembler/include/assembler.h
+++ Assembler/include/assembler.h
@@ -12,7 +12,8 @@ enum Errors_of_ASM
     ERROR_OF_CREATING_OUT_FILE = 4,
     ERROR_OF_DESTRUCTOR        = 5,
     ERROR_OF_CREATE_ARRAY      = 6,
-    ERROR_OF_UNKNOWN_REGISTER  = 7
+    ERROR_OF_UNKNOWN_REGISTER  = 7,
+    ERROR_OF_CONSTRUCTOR       = 8
 };
 //fix
 enum Label_name
@@ -38,7 +39,7 @@ struct Label
 
 struct Table_labels
 {
-    Label *labels;
+    struct Label *labels;
     size_t size_of_labels;
 };
 
@@ -49,9 +50,9 @@ struct ASM
     struct Table_labels *table;
 };
 
-Errors_of_ASM get_commands(struct Command *commands, size_t count_of_rows, FILE *file_pointer, struct Table_labels *table);
-Errors_of_ASM transform_commands(struct Command *commands, size_t count_of_rows);
-Errors_of_ASM create_file_with_commands(struct Command *commands, size_t count_of_rows);
+Errors_of_ASM get_commands(struct ASM *Asm, FILE *file_pointer);
+Errors_of_ASM transform_commands(struct ASM *Asm);
+Errors_of_ASM create_file_with_commands(struct ASM *Asm);
 
 #endif
 
--- Assembler/source/assembler.cpp
+++ Assembler/source/assembler.cpp
@@ -13,42 +13,43 @@
 static const int num_of_labels = 10;
 
 //array of structures
-static Errors_of_ASM destructor(struct Command *commands);
+static Errors_of_ASM constructor(struct ASM *Asm);
+static Errors_of_ASM destructor(struct ASM *Asm);
 
-Errors_of_ASM get_commands(struct Command *commands, size_t count_of_rows, FILE *file_pointer, struct Table_labels *table)
+Errors_of_ASM get_commands(struct ASM *Asm, FILE *file_pointer)
 {
-    if (commands == NULL || file_pointer == NULL)
+    if (Asm == NULL || file_pointer == NULL)
     {
         return ERROR_OF_READING_FROM_FILE;
     }
     size_t i = 0;
     int step = 0;
-    while (i < count_of_rows && fscanf(file_pointer, "%s", commands[i].command))
+    while (i < (Asm->count_of_rows) && fscanf(file_pointer, "%s", (Asm->commands)[i].command))
     {
-        size_t len = strlen(commands[i].command);
+        size_t len = strlen((Asm->commands)[i].command);
         //parse_word()
         //parse_label()
-        if (commands[i].command[len - 1] == ':')
+        if ((Asm->commands)[i].command[len - 1] == ':')
         {
             step += 1;
-            for (size_t j = 0; j < table->size_of_labels; j++)
+            for (size_t j = 0; j < (Asm->table)->size_of_labels; j++)
             {
-                if ((table->labels)[j].name == EMPTY)
+                if (((Asm->table)->labels)[j].name == EMPTY)
                 {
-                    if (strcasecmp(commands[i].command, "NEXT:") == 0)
+                    if (strcasecmp((Asm->commands)[i].command, "NEXT:") == 0)
                     {
-                        (table->labels)[j].name = NEXT;
-                        (table->labels)[j].address = step;
+                        ((Asm->table)->labels)[j].name = NEXT;
+                        ((Asm->table)->labels)[j].address = step;
                     }
                 }
                 break;
             }
             char str[6] = "label";
-            memcpy(commands[i].command, str, 6);
+            memcpy((Asm->commands)[i].command, str, 6);
         }
 
-        if (strcasecmp(commands[i].command, "push") == 0
-         || strcasecmp(commands[i].command, "pop")  == 0)
+        if (strcasecmp((Asm->commands)[i].command, "push") == 0
+         || strcasecmp((Asm->commands)[i].command, "pop")  == 0)
         {
             //parse_push_pop_cmd()
             step += 1;
@@ -56,22 +57,22 @@ Errors_of_ASM get_commands(struct Command *commands, size_t count_of_rows, FILE 
             fscanf(file_pointer, "%s", s);
             if (isalpha(s[0]))
             {
-                commands[i].element = TOXIC;
+                (Asm->commands)[i].element = TOXIC;
                 if (strcasecmp(s, "ax") == 0)
                 {
-                    commands[i].reg = AX;
+                    (Asm->commands)[i].reg = AX;
                 }
                 else if (strcasecmp(s, "bx") == 0)
                 {
-                    commands[i].reg = BX;
+                    (Asm->commands)[i].reg = BX;
                 }
                 else if (strcasecmp(s, "cx") == 0)
                 {
-                    commands[i].reg = CX;
+                    (Asm->commands)[i].reg = CX;
                 }
                 else if (strcasecmp(s, "dx") == 0)
                 {
-                    commands[i].reg = DX;
+                    (Asm->commands)[i].reg = DX;
                 }
                 else
                 {
@@ -80,23 +81,23 @@ Errors_of_ASM get_commands(struct Command *commands, size_t count_of_rows, FILE 
             }
             else
             {
-                commands[i].element = atoi(s);
-                commands[i].reg = NOT_A_REGISTER;
+                (Asm->commands)[i].element = atoi(s);
+                (Asm->commands)[i].reg = NOT_A_REGISTER;
             }
         }
-        else if (strcasecmp(commands[i].command, "jmp")  == 0
-                || strcasecmp(commands[i].command, "ja")   == 0
-                || strcasecmp(commands[i].command, "jae")  == 0
-                || strcasecmp(commands[i].command, "jb")   == 0
-                || strcasecmp(commands[i].command, "jbe")  == 0
-                || strcasecmp(commands[i].command, "je")   == 0
-                || strcasecmp(commands[i].command, "jne")  == 0)
+        else if (strcasecmp((Asm->commands)[i].command, "jmp")    == 0
+                || strcasecmp((Asm->commands)[i].command, "ja")   == 0
+                || strcasecmp((Asm->commands)[i].command, "jae")  == 0
+                || strcasecmp((Asm->commands)[i].command, "jb")   == 0
+                || strcasecmp((Asm->commands)[i].command, "jbe")  == 0
+                || strcasecmp((Asm->commands)[i].command, "je")   == 0
+                || strcasecmp((Asm->commands)[i].command, "jne")  == 0)
         {
             //parse_jump_cmd()
             step += 1;
             char str[50] = {0};
             fscanf(file_pointer, "%s", str);
-            commands[i].reg = NOT_A_REGISTER;
+            (Asm->commands)[i].reg = NOT_A_REGISTER;
             Label_name label = EMPTY;
             if (strcasecmp(str, "NEXT:") == 0)
             {
@@ -106,11 +107,11 @@ Errors_of_ASM get_commands(struct Command *commands, size_t count_of_rows, FILE 
             {
                 label = SKIP;
             }
-            for (size_t k = 0; k < table->size_of_labels; k++)
+            for (size_t k = 0; k < (Asm->table)->size_of_labels; k++)
             {
-                if ((table->labels)[k].name == label)
+                if (((Asm->table)->labels)[k].name == label)
                 {
-                    commands[i].element = (table->labels)[k].address;
+                    (Asm->commands)[i].element = ((Asm->table)->labels)[k].address;
                     break;
                 }
             }
@@ -122,110 +123,110 @@ Errors_of_ASM get_commands(struct Command *commands, size_t count_of_rows, FILE 
 }
 
 
-Errors_of_ASM transform_commands(struct Command *commands, size_t count_of_rows)
+Errors_of_ASM transform_commands(struct ASM *Asm)
 {
-    if (commands == NULL)
+    if (Asm == NULL)
     {
         return ERROR_OF_NO_COMMANDS;
     }
-    for (size_t i = 0; i < count_of_rows; i++)
+    for (size_t i = 0; i < (Asm->count_of_rows); i++)
     {
-        if (strcasecmp(commands[i].command, "push") == 0)
+        if (strcasecmp((Asm->commands)[i].command, "push") == 0)
         {
-            commands[i].transformed_command = CMD_PUSH;
+            (Asm->commands)[i].transformed_command = CMD_PUSH;
         }
-        else if (strcasecmp(commands[i].command, "pop") == 0)
+        else if (strcasecmp((Asm->commands)[i].command, "pop") == 0)
         {
-            commands[i].transformed_command = CMD_POP;
+            (Asm->commands)[i].transformed_command = CMD_POP;
         }
-        else if (strcasecmp(commands[i].command, "add") == 0)
+        else if (strcasecmp((Asm->commands)[i].command, "add") == 0)
         {
-            commands[i].transformed_command = CMD_ADD;
+            (Asm->commands)[i].transformed_command = CMD_ADD;
         }
-        else if (strcasecmp(commands[i].command, "sub") == 0)
+        else if (strcasecmp((Asm->commands)[i].command, "sub") == 0)
         {
-            commands[i].transformed_command = CMD_SUB;
+            (Asm->commands)[i].transformed_command = CMD_SUB;
         }
-        else if (strcasecmp(commands[i].command, "mul") == 0)
+        else if (strcasecmp((Asm->commands)[i].command, "mul") == 0)
         {
-            commands[i].transformed_command = CMD_MUL;
+            (Asm->commands)[i].transformed_command = CMD_MUL;
         }
-        else if (strcasecmp(commands[i].command, "div") == 0)
+        else if (strcasecmp((Asm->commands)[i].command, "div") == 0)
         {
-            commands[i].transformed_command = CMD_DIV;
+            (Asm->commands)[i].transformed_command = CMD_DIV;
         }
-        else if (strcasecmp(commands[i].command, "out") == 0)
+        else if (strcasecmp((Asm->commands)[i].command, "out") == 0)
         {
-            commands[i].transformed_command = CMD_OUT;
+            (Asm->commands)[i].transformed_command = CMD_OUT;
         }
-        else if (strcasecmp(commands[i].command, "dump") == 0)
+        else if (strcasecmp((Asm->commands)[i].command, "dump") == 0)
         {
-            commands[i].transformed_command = CMD_DUMP;
+            (Asm->commands)[i].transformed_command = CMD_DUMP;
         }
-        else if (strcasecmp(commands[i].command, "in") == 0)
+        else if (strcasecmp((Asm->commands)[i].command, "in") == 0)
         {
-            commands[i].transformed_command = CMD_IN;
+            (Asm->commands)[i].transformed_command = CMD_IN;
         }
-        else if (strcasecmp(commands[i].command, "sqrt") == 0)
+        else if (strcasecmp((Asm->commands)[i].command, "sqrt") == 0)
         {
-            commands[i].transformed_command = CMD_SQRT;
+            (Asm->commands)[i].transformed_command = CMD_SQRT;
         }
-        else if (strcasecmp(commands[i].command, "sin") == 0)
+        else if (strcasecmp((Asm->commands)[i].command, "sin") == 0)
         {
-            commands[i].transformed_command = CMD_SIN;
+            (Asm->commands)[i].transformed_command = CMD_SIN;
         }
-        else if (strcasecmp(commands[i].command, "cos") == 0)
+        else if (strcasecmp((Asm->commands)[i].command, "cos") == 0)
         {
-            commands[i].transformed_command = CMD_COS;
+            (Asm->commands)[i].transformed_command = CMD_COS;
         }
-        else if (strcasecmp(commands[i].command, "jmp") == 0)
+        else if (strcasecmp((Asm->commands)[i].command, "jmp") == 0)
         {
-            commands[i].transformed_command = CMD_JMP;
+            (Asm->commands)[i].transformed_command = CMD_JMP;
         }
-        else if (strcasecmp(commands[i].command, "ja") == 0)
+        else if (strcasecmp((Asm->commands)[i].command, "ja") == 0)
         {
-            commands[i].transformed_command = CMD_JA;
+            (Asm->commands)[i].transformed_command = CMD_JA;
         }
-        else if (strcasecmp(commands[i].command, "jae") == 0)
+        else if (strcasecmp((Asm->commands)[i].command, "jae") == 0)
         {
-            commands[i].transformed_command = CMD_JAE;
+            (Asm->commands)[i].transformed_command = CMD_JAE;
         }
-        else if (strcasecmp(commands[i].command, "jb") == 0)
+        else if (strcasecmp((Asm->commands)[i].command, "jb") == 0)
         {
-            commands[i].transformed_command = CMD_JB;
+            (Asm->commands)[i].transformed_command = CMD_JB;
         }
-        else if (strcasecmp(commands[i].command, "jbe") == 0)
+        else if (strcasecmp((Asm->commands)[i].command, "jbe") == 0)
         {
-            commands[i].transformed_command = CMD_JBE;
+            (Asm->commands)[i].transformed_command = CMD_JBE;
         }
-        else if (strcasecmp(commands[i].command, "je") == 0)
+        else if (strcasecmp((Asm->commands)[i].command, "je") == 0)
         {
-            commands[i].transformed_command = CMD_JE;
+            (Asm->commands)[i].transformed_command = CMD_JE;
         }
-        else if (strcasecmp(commands[i].command, "jne") == 0)
+        else if (strcasecmp((Asm->commands)[i].command, "jne") == 0)
         {
-            commands[i].transformed_command = CMD_JNE;
+            (Asm->commands)[i].transformed_command = CMD_JNE;
         }
-        else if (strcasecmp(commands[i].command, "hlt") == 0)
+        else if (strcasecmp((Asm->commands)[i].command, "hlt") == 0)
         {
-            commands[i].transformed_command = CMD_HLT;
+            (Asm->commands)[i].transformed_command = CMD_HLT;
         }
-        else if (strcasecmp(commands[i].command, "label") == 0)
+        else if (strcasecmp((Asm->commands)[i].command, "label") == 0)
         {
-            commands[i].transformed_command = CMD_LABEL;
+            (Asm->commands)[i].transformed_command = CMD_LABEL;
         }
         else
         {
-            commands[i].transformed_command = CMD_UNKNOWN;
+            (Asm->commands)[i].transformed_command = CMD_UNKNOWN;
         }
     }
     return NO_ASM_ERRORS;
 }
 
-Errors_of_ASM create_file_with_commands(struct Command *commands, size_t count_of_rows)
+Errors_of_ASM create_file_with_commands(struct ASM *Asm)
 {
     FILE *fp = fopen("Assembler/source/cpu_commands.txt", "w");
-    if (commands == NULL || fp == NULL)
+    if (Asm == NULL || fp == NULL)
     {
         return ERROR_OF_CREATING_OUT_FILE;
     }
@@ -246,35 +247,61 @@ Errors_of_ASM create_file_with_commands(struct Command *commands, size_t count_o
     fprintf(fp, "Month: %s\n", month);
     fprintf(fp, "Time: %s\n", current_time);
     fprintf(fp, "________________________________________________________________\n");
-    for (size_t i = 0; i < count_of_rows; i++)
+    for (size_t i = 0; i < (Asm->count_of_rows); i++)
     {
-        if (commands[i].transformed_command == CMD_LABEL)
+        if ((Asm->commands)[i].transformed_command == CMD_LABEL)
         {
             continue;
         }
-        if (commands[i].transformed_command == CMD_PUSH
-         || commands[i].transformed_command == CMD_POP)
+
+        if ((Asm->commands)[i].transformed_command == CMD_PUSH
+         || (Asm->commands)[i].transformed_command == CMD_POP)
         {
-            fprintf(fp, "%d %d %d\n", commands[i].transformed_command, commands[i].element, commands[i].reg);
+            fprintf(fp, "%d %d %d\n", (Asm->commands)[i].transformed_command, (Asm->commands)[i].element, (Asm->commands)[i].reg);
         }
-        else if (commands[i].transformed_command == CMD_JMP
-              || commands[i].transformed_command == CMD_JA
-              || commands[i].transformed_command == CMD_JAE
-              || commands[i].transformed_command == CMD_JB
-              || commands[i].transformed_command == CMD_JBE
-              || commands[i].transformed_command == CMD_JE
-              || commands[i].transformed_command == CMD_JNE)
+        else if ((Asm->commands)[i].transformed_command == CMD_JMP
+              || (Asm->commands)[i].transformed_command == CMD_JA
+              || (Asm->commands)[i].transformed_command == CMD_JAE
+              || (Asm->commands)[i].transformed_command == CMD_JB
+              || (Asm->commands)[i].transformed_command == CMD_JBE
+              || (Asm->commands)[i].transformed_command == CMD_JE
+              || (Asm->commands)[i].transformed_command == CMD_JNE)
         {
-            fprintf(fp, "%d %d\n", commands[i].transformed_command, commands[i].element);
+            fprintf(fp, "%d %d\n", (Asm->commands)[i].transformed_command, (Asm->commands)[i].element);
         }
         else
         {
-            fprintf(fp, "%d\n", commands[i].transformed_command);
+            fprintf(fp, "%d\n", (Asm->commands)[i].transformed_command);
         }
     }
     return NO_ASM_ERRORS;
 }
 
+
+static Errors_of_ASM constructor(struct ASM *Asm)
+{
+    if (Asm == NULL)
+    {
+        return ERROR_OF_CONSTRUCTOR;
+    }
+    Asm->commands = (Command *) calloc(Asm->count_of_rows, sizeof(Command));
+    if (Asm->commands == NULL)
+    {
+        return ERROR_OF_CONSTRUCTOR;
+    }
+    Asm->table = (Table_labels *) calloc(1, sizeof(Table_labels));
+    if (Asm->table == NULL)
+    {
+        return ERROR_OF_CONSTRUCTOR;
+    }
+    (Asm->table)->size_of_labels = (size_t)num_of_labels;
+    (Asm->table)->labels = (Label *) calloc((Asm->table)->size_of_labels, sizeof(Label));
+    if ((Asm->table)->labels == NULL)
+    {
+        return ERROR_OF_CONSTRUCTOR;
+    }
+    return NO_ASM_ERRORS;
+}
 
 
 int main()
@@ -292,42 +319,33 @@ int main()
         }
     }
     rewind(fp);
-
-    // c...tor
-    struct Command *commands = (Command *) calloc(count_of_rows, sizeof(Command));
-    struct Table_labels table = {0};
-    table.size_of_labels = (size_t)num_of_labels;
-    table.labels = (Label *) calloc(table.size_of_labels, sizeof(Label));
-
-    Errors_of_ASM error = NO_ASM_ERRORS;
-    if (commands == NULL)
-    {
-        error = ERROR_OF_CREATE_ARRAY;
-    }
+    struct ASM Asm = {0};
+    Asm.count_of_rows = count_of_rows;
+    Errors_of_ASM error = constructor(&Asm);
     if (error != NO_ASM_ERRORS)
     {
         fprintf(stderr, "error=%d\n", error);
         return 0;
     }
-    error = get_commands(commands, count_of_rows, fp, &table);
+    error = get_commands(&Asm, fp);
     if (error != NO_ASM_ERRORS)
     {
         fprintf(stderr, "error=%d\n", error);
         return 0;
     }
-    error = transform_commands(commands, count_of_rows);
+    error = transform_commands(&Asm);
     if (error != NO_ASM_ERRORS)
     {
         fprintf(stderr, "error=%d\n", error);
         return 0;
     }
-    error = create_file_with_commands(commands, count_of_rows);
+    error = create_file_with_commands(&Asm);
     if (error != NO_ASM_ERRORS)
     {
         fprintf(stderr, "error=%d\n", error);
         return 0;
     }
-    error = destructor(commands);
+    error = destructor(&Asm);
     if (error != NO_ASM_ERRORS)
     {
         fprintf(stderr, "error=%d\n", error);
@@ -339,12 +357,16 @@ int main()
 
 
 
-static Errors_of_ASM destructor(struct Command *commands)
+static Errors_of_ASM destructor(struct ASM *Asm)
 {
-    if (commands == NULL)
+    if (Asm == NULL)
     {
         return ERROR_OF_DESTRUCTOR;
     }
-    free(commands);
+    Asm->count_of_rows = 0;
+    free(Asm->commands);
+    (Asm->table)->size_of_labels = 0;
+    free((Asm->table)->labels);
+    free(Asm->table);
     return NO_ASM_ERRORS;
 }
